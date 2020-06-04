571a906c5fd0736084fb14db6d64176d09a042e9
apcom <apcom@live.ru>
2020-06-04 11:12:01 +0300

    build

---
+++ build/precache-manifest.2f8e44df5ad9d1a7fc04c85ccaa9bdd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc0eb27759e786e452e7e1bfcd74c847",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acfcbbe5cc09515b829a",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "54db3f7913a6ba490ee0",
+    "url": "/static/js/2.507d5ae0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.507d5ae0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acfcbbe5cc09515b829a",
+    "url": "/static/js/main.212db994.chunk.js"
+  },
+  {
+    "revision": "8fe18681051f94ed883a",
+    "url": "/static/js/runtime-main.a0c9bbf5.js"
+  }
+]);
